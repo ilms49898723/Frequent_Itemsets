55ca02412e72efa6187226f02d2abb8c8d6860ee
littlebird <ilms49898723@gmail.com>
2017-06-07 22:49:40 +0900

    add solver

--- src/main/java/com/github/ilms49898723/frequentitemsets/PCY/PCYCandidateGenerator.java
+++
@@ -1,5 +0,0 @@
-package com.github.ilms49898723.frequentitemsets.PCY;
-
-public class PCYCandidateGenerator {
-
-}
--- src/main/java/com/github/ilms49898723/frequentitemsets/PCY/PCYSolver.java
+++ src/main/java/com/github/ilms49898723/frequentitemsets/PCY/PCYSolver.java
@@ -1,11 +1,24 @@
 package com.github.ilms49898723.frequentitemsets.PCY;
 
-import org.apache.hadoop.io.WritableComparable;
+import com.github.ilms49898723.frequentitemsets.Main;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class PCYSolver {
     private static class PCYKey implements WritableComparable<PCYKey> {
@@ -49,5 +62,225 @@ public class PCYSolver {
                 mKeys.add(in.readInt());
             }
         }
+    }
+
+    private static class PCYValue implements Writable {
+        private ArrayList<Integer> mValues;
+
+        public PCYValue() {
+            mValues = new ArrayList<>();
+        }
+
+        public ArrayList<Integer> getValues() {
+            return mValues;
+        }
+
+        @Override
+        public void write(DataOutput out) throws IOException {
+            out.writeInt(mValues.size());
+            for (int value: mValues) {
+                out.writeInt(value);
+            }
+        }
+
+        @Override
+        public void readFields(DataInput in) throws IOException {
+            mValues = new ArrayList<>();
+            int size = in.readInt();
+            for (int i = 0; i < size; ++i) {
+                mValues.add(in.readInt());
+            }
+        }
+    }
+
+    private static class SolverMapper extends Mapper<Object, Text, PCYKey, PCYValue> {
+        private static final int HASHTABLE_SIZE = 10007;
+
+        private HashSet<Integer> mHashes;
+        private HashSet<Integer> mFrequentItems;
+        private ArrayList<Integer> mElements;
+        private Context mContext;
+        private int mK;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+            mK = context.getConfiguration().getInt("k", -1);
+            mHashes = new HashSet<>();
+            mFrequentItems = new HashSet<>();
+            initializeHashes(context);
+            initializeFrequentItems(context);
+        }
+
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] tokens = value.toString().split(" ");
+            mContext = context;
+            mElements = new ArrayList<>();
+            for (String token : tokens) {
+                mElements.add(Integer.parseInt(token));
+            }
+            mElements.sort(Integer::compareTo);
+            generateSets(0, 0, new ArrayList<>());
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            super.cleanup(context);
+        }
+
+        private void initializeHashes(Context context) {
+            try {
+                int k = context.getConfiguration().getInt("k", -1) - 1;
+                FileSystem fileSystem = FileSystem.get(context.getConfiguration());
+                FileStatus[] fileStatuses = fileSystem.listStatus(new Path("frequent-itemsets-" + k));
+                for (FileStatus fileStatus : fileStatuses) {
+                    String[] fileName = fileStatus.getPath().toString().split("/");
+                    if (fileName[fileName.length - 1].contains("candidates")) {
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(fileSystem.open(fileStatus.getPath()))
+                        );
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            mHashes.add(Integer.parseInt(line));
+                        }
+                        reader.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void initializeFrequentItems(Context context) {
+            try {
+                FileSystem fileSystem = FileSystem.get(context.getConfiguration());
+                FileStatus[] fileStatuses = fileSystem.listStatus(new Path("frequent-itemsets-1"));
+                for (FileStatus fileStatus : fileStatuses) {
+                    String[] fileName = fileStatus.getPath().toString().split("/");
+                    if (fileName[fileName.length - 1].contains("itemsets")) {
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(fileSystem.open(fileStatus.getPath()))
+                        );
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            mFrequentItems.add(Integer.parseInt(line));
+                        }
+                        reader.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void generateSets(int now, int index, ArrayList<Integer> elements) throws IOException, InterruptedException {
+            if (now == mK) {
+                PCYKey newKey = new PCYKey();
+                int hashValue = PCYUtil.hash(newKey.getKeys(), HASHTABLE_SIZE);
+                if (mHashes.contains(hashValue)) {
+                    PCYValue newValue = new PCYValue();
+                    newKey.getKeys().addAll(elements);
+                    for (int value : mElements) {
+                        if (!newKey.getKeys().contains(value) && mFrequentItems.contains(value)) {
+                            newValue.getValues().add(value);
+                        }
+                    }
+                    mContext.write(newKey, newValue);
+                }
+                return;
+            }
+            for (int i = index; i < mElements.size(); ++i) {
+                elements.add(mElements.get(i));
+                generateSets(now + 1, i + 1, elements);
+                elements.remove(elements.size() - 1);
+            }
+        }
+    }
+
+    private static class SolverReducer extends Reducer<PCYKey, PCYValue, NullWritable, Text> {
+        private static final int HASHTABLE_SIZE = 10007;
+
+        private MultipleOutputs<NullWritable, Text> mMultipleOutputs;
+        private int[] mHashTable;
+        private int mThreshold;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+            Configuration conf = context.getConfiguration();
+            mMultipleOutputs = new MultipleOutputs<>(context);
+            mHashTable = new int[HASHTABLE_SIZE];
+            mThreshold = conf.getInt("threshold", Integer.MAX_VALUE);
+            Arrays.fill(mHashTable, 0);
+        }
+
+        @Override
+        protected void reduce(PCYKey key, Iterable<PCYValue> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            int[] localHashTable = new int[HASHTABLE_SIZE];
+            Arrays.fill(localHashTable, 0);
+            for (PCYValue value : values) {
+                for (int element : value.getValues()) {
+                    ArrayList<Integer> candidate = new ArrayList<>();
+                    candidate.addAll(key.getKeys());
+                    candidate.add(element);
+                    candidate.sort(Integer::compareTo);
+                    localHashTable[PCYUtil.hash(candidate, HASHTABLE_SIZE)] += 1;
+                }
+                ++count;
+            }
+            if (count >= mThreshold) {
+                String output = "";
+                boolean isFirst = true;
+                for (int element : key.getKeys()) {
+                    output += String.valueOf(element);
+                    if (!isFirst) {
+                        output += " ";
+                    } else {
+                        isFirst = false;
+                    }
+                }
+                mMultipleOutputs.write("itemsets", NullWritable.get(), new Text(output));
+                for (int i = 0; i < HASHTABLE_SIZE; ++i) {
+                    mHashTable[i] += localHashTable[i];
+                }
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            super.cleanup(context);
+            for (int i = 0; i < HASHTABLE_SIZE; ++i) {
+                if (mHashTable[i] >= mThreshold) {
+                    mMultipleOutputs.write("candidate", NullWritable.get(), new Text(Integer.toString(i)));
+                }
+            }
+            mMultipleOutputs.close();
+        }
+    }
+
+    public static void run(int k, int n, int threshold, String input) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Configuration jConf = new Configuration(configuration);
+        jConf.setInt("n", n);
+        jConf.setInt("k", k);
+        jConf.setInt("threshold", threshold);
+        Job job = Job.getInstance(jConf, "Frequent Itemsets Pass " + k);
+        job.setJarByClass(Main.class);
+        job.setMapOutputKeyClass(PCYKey.class);
+        job.setMapOutputValueClass(PCYValue.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setMapperClass(SolverMapper.class);
+        job.setReducerClass(SolverReducer.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setNumReduceTasks(1);
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path("frequent-itemsets-" + k));
+        MultipleOutputs.addNamedOutput(job, "candidates", TextOutputFormat.class, NullWritable.class, Text.class);
+        MultipleOutputs.addNamedOutput(job, "itemsets", TextOutputFormat.class, NullWritable.class, Text.class);
+        job.waitForCompletion(true);
     }
 }
